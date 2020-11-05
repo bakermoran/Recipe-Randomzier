343dc51ef8b1c2db18d35cc8819a09de14373c5a
Baker Moran <baker.moran@joinroot.com>
2020-11-05 16:35:10 -0500

    get basic react app working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :recipes
+    end
+  end
+  root 'home#index'
+  get '/*path' => 'home#index'
+  get '*page', to: 'home#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
 end
