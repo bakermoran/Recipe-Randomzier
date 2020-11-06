927b26418f870af8922c852902e311fd8f416888
Baker Moran <baker.moran@joinroot.com>
2020-11-05 20:22:39 -0500

    recipes api actions

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::RecipesController < ApplicationController
+    def index
+        recipes = Recipe.all
+        render json: recipes
+    end
+
+    def show
+        @recipe = Recipe.find(params[:id])
+        if @recipe
+            render json: @recipe
+        else
+            render json: @recipe.errors
+        end
+    end
+
+    def create
+        @recipe = Recipe.create!(recipe_params)
+        if @recipe
+            render json: @recipe
+        else
+            render json: @recipe.errors
+        end
+    end
+
+    def destroy
+        recipe&.destroy
+        render json: { message: 'Recipe deleted' }
+    end
+
+    private
+
+    def recipe_params
+        params.permit(:name, :image_url, :ingredients, :instructions)
+    end
 end
